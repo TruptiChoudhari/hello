bd0e85b8533247efe51d7cde3fd0af8d716fc8a5
TruptiChoudhari <trupti.choudhari1998@gmail.com>
2023-10-16 18:45:04 +0530

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author LENOVO
+ *
+ */
+/**
+ * @author LENOVO
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello");
+
+
+	}
+
+}
